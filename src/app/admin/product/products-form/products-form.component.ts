e5d62d2e7261b0c492e7d68ea2ab2ce712554d71
Giuseppe <mazzeogiu@gmail.com>
2021-07-29 17:11:47 +0200

    restyling

--- src/app/admin/product/products-form/products-form.component.ts
+++ src/app/admin/product/products-form/products-form.component.ts
@@ -1,7 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
+import { CompanyApiService } from '../../api/companyApi.service';
 import { ProductApiService } from '../../api/productApi.services';
 import { ProductTypeService } from '../../api/productType-api.service';
+import { Company } from '../../models/company.model';
 import { Product } from '../../models/product.model';
 import { ProductType } from '../../models/productType.model';
 
@@ -12,17 +14,19 @@ import { ProductType } from '../../models/productType.model';
 })
 export class ProductsFormComponent implements OnInit {
 
+  companies: Company[] = [];
   productsTypes: ProductType[] = [];
   @Input() product!: Product;
   
 
   
-  constructor(private productApiService: ProductApiService, private productTypeService: ProductTypeService, private router: Router, private activatedRouter : ActivatedRoute) { 
+  constructor(private companyApiService: CompanyApiService, private productApiService: ProductApiService, private productTypeService: ProductTypeService, private router: Router, private activatedRouter : ActivatedRoute) { 
     
   }
   
   ngOnInit(): void {
     
+    this.getCompanies();
     this.getProductsTypes();
     this.getProduit();
   }
@@ -44,6 +48,10 @@ export class ProductsFormComponent implements OnInit {
   getProductsTypes(): void {
     this.productTypeService.getAllProductType().subscribe(elements => this.productsTypes = elements);
     
+  }
+
+  getCompanies(): void {
+    this.companyApiService.getCompanies().subscribe(elements => this.companies = elements);
   }
 
 
